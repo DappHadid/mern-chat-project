1aa1b5e2f51ed73713bceb4c7e936a46f5fdaa75
DappHadid <154480498+DappHadid@users.noreply.github.com>
2026-02-28 23:35:08 +0700

    feat: Initialize Project

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="bg-deep-blue min-h-screen flex items-center justify-center">
       <h1 className="text-crisp-white text-3xl font-bold">
-        Project Chat <span className="text-pastel-green">Berjalan!</span>
+        Project Chat <span className="text-pastel-green">Berjalan! HOREE</span>
       </h1>
     </div>
   );
